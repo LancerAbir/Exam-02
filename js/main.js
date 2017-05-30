2a8b0c011a2ff7d9ec8f2b91ce2977a6bc4fb5de
abir2665266 <dhar.abir.kumar@gmail.com>
2017-05-30 21:11:04 +0600

    Exam
    02

---
+++ js/main.js
@@ -0,0 +1,36 @@
+(function($){
+    'use strict';
+    
+    jQuery(document).ready(function($){
+        
+        
+        $(".video-play-btn").magnificPopup({
+            type : 'video',
+        });
+        
+        
+        
+        $(".testimonials-list").owlCarousel({
+            items : 1,
+            navs : false,
+            dots : true,
+            loop : true,
+            autoplay : false,
+        })
+        
+        
+        
+        
+        
+        
+    });
+    
+    
+    jQuery(window).load(function($){
+        
+        
+        
+    });
+    
+    
+})(jQuery);
